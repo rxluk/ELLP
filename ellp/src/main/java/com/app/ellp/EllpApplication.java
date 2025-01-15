1b76701e028ae2b105101e6296569ba3fe3e8111
Lucas G. Costa <112594566+rxluk@users.noreply.github.com>
2025-01-15 13:18:24 -0300

    Add files via upload

---
+++ ellp/src/main/java/com/app/ellp/EllpApplication.java
@@ -0,0 +1,13 @@
+package com.app.ellp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EllpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EllpApplication.class, args);
+	}
+
+}
---
+++ ellp/src/test/java/com/app/ellp/EllpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.ellp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EllpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
